a978b25ed8c3babbfe34f1b9241cf1315fb3551a
prettylikeme <103903530@qq.com>
2025-06-05 05:45:39 +0000

    GitHub Actions Auto Builder at 2025-06-05 05:45:39

--- docs/assets/index.md.YUzotgwr.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./chunks/framework.C77v9uM6.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"发里福笑导航"}},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),n={name:"index.md"};function o(r,s,c,d,i,m){return a(),t("div")}const f=e(n,[["render",o]]);export{_ as __pageData,f as default};
